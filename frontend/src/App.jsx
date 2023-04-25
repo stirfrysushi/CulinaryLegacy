d62413abca774c8d4bb3d196770df153461a587a
An <apn2@buffalo.edu>
2023-04-24 23:44:35 -0400

    front page for now

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import logo from './assets/logo.png'
 import './App.css'
+import {Routes, Route, useNavigate} from 'react-router-dom';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,27 +9,20 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <a href="http://127.0.0.1:5173/" target="_blank">
+          <img src={logo} className="logo" alt="logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Welcome to CulinaryLegacy ٩(◕‿◕｡)۶</h1>
+
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button onClick={() => hrefFunction()}>
+          Login Here
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
+
 
 export default App

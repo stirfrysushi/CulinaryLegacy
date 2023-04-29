d63b5cd1a7325dd785a0d9174138a6fc3bf01f4f
anht-11 <kyanhvn11@gmail.com>
2023-04-29 16:16:58 -0400

    modify data defintion and update some functions

--- culinary-contract/contracts/Culinary.sol
+++ culinary-contract/contracts/Culinary.sol
@@ -3,18 +3,45 @@ pragma solidity ^0.8.9;
 
 contract CulinaryLegacyRecipe {
 
-    // structs definition + data 
-    struct Recipe {
+    //###########Data#####################
+	struct Recipe {
         uint recipeID; 
-        uint price; 
+        uint price;
     }
     struct User {
-        uint userID; 
-        bool registered; 
+        uint userID;
+        bool registered;
     }
-    uint index; 
-    mapping(uint => User) owners; 
-    mapping(address => User) registeredUser; 
-    address contract_owner; 
+
+    uint index;
+    mapping(Recipe => User) owners;//TODO: must be elementary type name for mapping
+    address contract_owner;
+    mapping(address => User) registeredUser
+    //#########Event and Modifier##############
+    event recipeRequestCompleted(...)
+    modifier onlyContractOwner(){
+        require(msg.sender == contract_owner);
+        _;
+    }
+    modifier onlyRecipeOwner(...)
+    //#########Event and Modifier##############
+
+    //#########Functions###############
+    function register() public {
+        address user = msg.sender;
+        registeredUser[user].registered = true;
+        registeredUser[user].userID = index;
+        index.increment();
+    }
+    function unregister(uint userID) //Do we need this?
+    function request(uint recipeID)
+    function response(uint recipeID) onlyRecipeOwner
+    function view(uint recipeID)
+    function sell(uint recipeID)
+    function buy(uint recipeID)
+    function terminateContract () onlyContractOwner{
+        
+    }
+    function unregisterMember(uint userID) onlyContractOwner
 
 }

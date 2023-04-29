c4e9a6863c2706b10fd50b05eeb46efaaf048bdd
anht-11 <kyanhvn11@gmail.com>
2023-04-29 18:46:26 -0400

    update data structure to map creator to their own recipe

--- culinary-contract/contracts/Culinary.sol
+++ culinary-contract/contracts/Culinary.sol
@@ -1,47 +1,61 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.9;
-
-contract CulinaryLegacyRecipe {
-
-    //###########Data#####################
+//https://ethereum.stackexchange.com/questions/9142/how-to-convert-a-string-to-bytes32
+//https://ethereum.stackexchange.com/questions/2519/how-to-convert-a-bytes32-to-string
+pragma solidity >=0.4.0 <0.6.0;
+contract CulinaryLegacyRecipe{
+    //##############Data Structure#####################
 	struct Recipe {
         uint recipeID; 
         uint price;
     }
     struct User {
-        uint userID;
+        uint[] recipeOwned //recipe owned by this user
         bool registered;
     }
 
-    uint index;
-    mapping(Recipe => User) owners;//TODO: must be elementary type name for mapping
+    uint index; //increment id for recipe
+    //mapping(Recipe => address) owners;//TODO: must be elementary type name for mapping
     address contract_owner;
-    mapping(address => User) registeredUser
+    mapping(address => User) registeredUser //keeping track registered user
+    mapping(address => Recipe) recipeCreator //keeping track of the creator of the recipe
+
     //#########Event and Modifier##############
     event recipeRequestCompleted(...)
     modifier onlyContractOwner(){
         require(msg.sender == contract_owner);
         _;
     }
-    modifier onlyRecipeOwner(...)
-    //#########Event and Modifier##############
-
+    modifier onlyRecipeOwner(uint recipdeId){
+        require(msg.sender == recipeCreator[recipdeId]);
+        _;
+    }
+    modifier onlyRegisteredUser(){
+        require(registeredUser[msg.sender].registered);
+        _;
+    }
     //#########Functions###############
     function register() public {
         address user = msg.sender;
         registeredUser[user].registered = true;
-        registeredUser[user].userID = index;
-        index.increment();
     }
     function unregister(uint userID) //Do we need this?
-    function request(uint recipeID)
+
+    //Create new recipe for sale
+    function addRecipe(uint price) onlyRegisteredUser public {
+        Recipe recipe = Recipe(index, price)
+        index.increment()
+        recipeCreator[msg.sender] = recipe
+    }
+    
+    function request(uint recipeID) {
+    }
     function response(uint recipeID) onlyRecipeOwner
     function view(uint recipeID)
     function sell(uint recipeID)
     function buy(uint recipeID)
     function terminateContract () onlyContractOwner{
-        
+        selfdestruct(contract_owner);
     }
     function unregisterMember(uint userID) onlyContractOwner
+
 
 }

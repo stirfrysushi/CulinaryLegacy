fdab277225da8b98b35b178dab4b1bbc0c8a40fe
An <apn2@buffalo.edu>
2023-05-13 19:08:25 -0400

    almost deployable

--- culinary-contract/migrations/1_initial_migration.js
+++ culinary-contract/migrations/1_initial_migration.js
@@ -1,5 +1,9 @@
 const Migrations = artifacts.require("Migrations");
 
 module.exports = function(deployer) {
-  deployer.deploy(Migrations);
+  deployer.deploy(Migrations, {
+    from: "0x9350dc8F330190d89f74de744765bb950428FF97",
+    gas: "1000000",
+    gasPrice: "9000000000",
+  });
 };
--- culinary-contract/truffle-config.js
+++ culinary-contract/truffle-config.js
@@ -1,5 +1,16 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const infuraKey = "0661998c22d242819a3132bcc27b640b";
+const mnemonic = "catch humble correct other version cotton teach banner advance cluster pass jazz" ; // Seed phrase
+
 module.exports = {
   networks: {
+    goerli: {
+      provider: () => new HDWalletProvider(mnemonic, `https://goerli.infura.io/v3/${infuraKey}`),
+      from: "0x9350dc8F330190d89f74de744765bb950428FF97",
+      network_id: 5, // Goerli's network ID
+      gas: 5500000,
+    },
+
     development: {
       host: "127.0.0.1",
       port: 7545,

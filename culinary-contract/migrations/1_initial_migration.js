bcb52b17cddcb90419482790bea7ab9e0dd27cb8
An <apn2@buffalo.edu>
2023-05-13 19:40:04 -0400

    final version

--- culinary-contract/migrations/1_initial_migration.js
+++ culinary-contract/migrations/1_initial_migration.js
@@ -1,9 +1,5 @@
 const Migrations = artifacts.require("Migrations");
 
 module.exports = function(deployer) {
-  deployer.deploy(Migrations, {
-    from: "0x9350dc8F330190d89f74de744765bb950428FF97",
-    gas: "1000000",
-    gasPrice: "9000000000",
-  });
+  deployer.deploy(Migrations); 
 };

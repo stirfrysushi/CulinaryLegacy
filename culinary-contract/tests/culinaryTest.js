ddf4948838e92c3413601360fa752850f1c03275
An <apn2@buffalo.edu>
2023-05-13 14:58:54 -0400

    test

--- culinary-contract/tests/culinaryTest.js
+++ culinary-contract/tests/culinaryTest.js
@@ -1,7 +1,24 @@
 const Culinary = artifacts.require('../contracts/Culinary.sol')
 const truffleAssert = require('truffle-assertions');
 
-contract('Culinary', function () {
+
+contract('Culinary', function (accounts) {
     let culinary
-  })
-  
+    
+    // initialization 
+    beforeEach('Setup Culinary Contract', async function () {
+      culinary = await Culinary.new();
+      console.log('done');
+    });
+
+    // example test 
+    describe('Initialization', async ()=>{
+      it('Success on initialization to bidding phase.', async function () {
+        let count = await culinary.recipeCounts();
+        assert.equal(count, 0);
+      });
+    });
+}
+
+// REMEMBER -> npm i 
+// THEN     -> truffle test
